cfe14834ed8c5393a1a0972edf04d82d21eb8a86
HAFIZ UMER MEHMOOD <umerhafiz2000@gmail.com>
2025-10-15 12:40:28 +0500

    Delete sdmmag.py

--- sdmmag.py
+++
@@ -1,6 +0,0 @@
-from curl_cffi import requests
-
-url = "https://www.sdmmag.com/rss/topic/6802-cybersecurity-chronicles"
-r = requests.get(url, impersonate="chrome")
-print(r.status_code, len(r.text))
-print(r.text[:100000])
